5d767916b4afeda2d5250b735b18af5edeef6dd0
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-11-12 18:37:36 +0000

    structure of products and css added

---
+++ my-app/src/pages/shop/product.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Product = (props) =>{
+    const {id, productName, price, productImage} = props.data;
+
+    return (
+      <div className="product">
+        <img src={productImage} />
+        <div className="description">
+          <p>
+            <b>{productName}</b>
+          </p>
+          <p>£{price}</p>
+          <button className="addCartBttn">Add to Cart</button>
+        </div>
+      </div>
+    );
+};
