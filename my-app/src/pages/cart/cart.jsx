925c88367d92d6a3408c22eb31ebcf91a5cb51ae
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-11-12 21:27:52 +0000

    cart structured

--- my-app/src/pages/cart/cart.jsx
+++ my-app/src/pages/cart/cart.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import {ShopContext} from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import {CartItem} from "../cart/cart-item";
+import "./cart.css";
 
 export const Cart = ( ) =>{
-return <div>
-    cart
+
+const{ cartItems} = useContext (ShopContext);
+
+return (
+<div className="cart">
+    <div>
+        <h1>
+           Your Cart Item 
+        </h1>
+    </div>
+    <div className="cartItems">
+       {PRODUCTS.map((product) =>{
+        if(cartItems[product.id] !== 0) {
+            return <CartItem data={product}/>
+        }
+
+       })}
+    </div>
+    
 </div>
+);
 };
--- my-app/src/pages/shop/product.jsx
+++ my-app/src/pages/shop/product.jsx
@@ -4,8 +4,10 @@ import { ShopContext } from "../../context/shop-context";
 
 export const Product = (props) =>{
     const { id, productName, price, productImage } = props.data;
-    const{ addToCart } = useContext(ShopContext);
+    const{ addToCart, cartItems } = useContext(ShopContext);
 
+
+    const cartItemsAmount = cartItems[id]
     return (
       <div className="product">
         <img src={productImage}  alt = " " />
@@ -16,7 +18,7 @@ export const Product = (props) =>{
           <p>£{price}</p>
           </div>
           <button className="addCartBttn" onClick={() => addToCart(id)}>
-            Add to Cart
+            Add to Cart {cartItemsAmount > 0 && <> ({cartItemsAmount})</>}
           </button>
         </div>
 
