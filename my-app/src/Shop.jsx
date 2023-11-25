bc4617c67f2c10845e3f85c1a693eba93206d7f4
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-11-25 19:58:48 +0000

    folders re arranged

---
+++ my-app/src/Shop.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { PRODUCTS } from "./Products.js";
+import { Product } from "./Product.jsx";
+import "./Shop.css";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1> Gabi s Shop </h1>
+      </div>
+
+      <div className="products">
+        {"  "}
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
